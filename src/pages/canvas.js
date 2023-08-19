ef64de566ef4371678796f67758686e80f126918
Gabriel Groppo <gabrielmgruppogg@gmail.com>
2023-08-19 18:34:26 -0300

    tcc

--- src/pages/canvas.js
+++ src/pages/canvas.js
@@ -2,11 +2,12 @@ import React from 'react';
 import '@excalidraw/excalidraw';
 import { Excalidraw } from '@excalidraw/excalidraw';
 import styles from './canvas.module.css';
+import './excalidraw.custom.css'
 
 function Canvas() {
   return (
     <div className={styles.Canvas}>
-      <Excalidraw />
+      <Excalidraw gridModeEnabled />
     </div>
   );
 }
