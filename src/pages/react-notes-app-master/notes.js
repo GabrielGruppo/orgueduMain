4efc64611707825a289f6de2592136e242361307
GabrielGruppo <gabrielmgruppogg@gmail.com>
2023-11-06 08:11:32 -0300

    tst

--- src/pages/react-notes-app-master/notes.js
+++ src/pages/react-notes-app-master/notes.js
@@ -14,20 +14,21 @@ const App = () => {
 
 		const acao = {acao:'busca',id_user: localStorage.getItem("id_user")};
 
-		fetch('http://localhost:84/orgueduMain/nota_repositorio.php', {
-			method: 'POST',
-			headers: {
-			  'Content-Type': 'application/json',
-			},
-			body: JSON.stringify(acao),
-		  })
-		  .then((response) => response.json())
-		  .then((response) => {
-			const savedNotes =  response;
-  			if(savedNotes){
-				setNotes(savedNotes)};
-		  })
-
+		if(localStorage.getItem('login')){ 
+			fetch('http://localhost:84/orgueduMain/nota_repositorio.php', {
+				method: 'POST',
+				headers: {
+				'Content-Type': 'application/json',
+				},
+				body: JSON.stringify(acao),
+			})
+			.then((response) => response.json())
+			.then((response) => {
+				const savedNotes =  response;
+				if(savedNotes){
+					setNotes(savedNotes)};
+			})
+		}
 	}, []);
 
 
