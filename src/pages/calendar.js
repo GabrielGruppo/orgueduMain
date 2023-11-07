beb157f2cce463bb5de82d8f02ee5cb37672a735
DedeRuindadePura <familiamiranda1101@gmail.com>
2023-11-07 11:59:39 -0300

    ghfghf

--- src/pages/calendar.js
+++ src/pages/calendar.js
@@ -3,12 +3,38 @@ import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import events from "./events";
 import "react-big-calendar/lib/css/react-big-calendar.css";
+import { useEffect } from "react";
 
 moment.locale("pt-BR");
 const localizer = momentLocalizer(moment);
 
 export default function ReactBigCalendar() {
   const [eventsData, setEventsData] = useState(events);
+
+  
+  useEffect(() => {
+
+      const data = {acao:'busca',user_id: localStorage.getItem("user_id")};
+
+			fetch('http://localhost:84/orgueduMain/event_repositorio.php', {
+				method: 'POST',
+				headers: {
+				'Content-Type': 'application/json',
+				},
+				body: JSON.stringify(data),
+			})
+			.then((response) => response.json())
+			.then((response) => {
+				for(let i=0;i<response.lenght;i++){
+          console.log(response[i]);
+        }
+			})
+			.catch(
+				console.log()
+			);
+
+  }, []);
+
   const handleSelect = ({ start, end }) => {
     console.log(start);
     console.log(end);
--- src/pages/react-notes-app-master/notes.js
+++ src/pages/react-notes-app-master/notes.js
@@ -25,7 +25,7 @@ const App = () => {
 					setNotes(savedNotes)};
 			})
 			.catch(
-				console.log('usuario nao logado')
+				console.log()
 			);
 
 	}, []);
