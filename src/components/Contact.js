e9452db227196e42fbad3115a24b08b02a3d889f
GabrielGruppo <gabrielmgruppogg@gmail.com>
2023-12-01 18:53:51 -0300

    tst

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,18 @@
 import React, { useState } from 'react';
 import styles from './Contact.module.css';
 import contactImg from '../images/contact.png';
+import emailjs from '@emailjs/browser';
+
+const sendEmail = (e) => {
+  e.preventDefault();
+
+  emailjs.sendForm('gmail', 'template_trljat8', e.target, 'CCNFkGgFPntNCurBp')
+    .then((result) => {
+        console.log(result.text);
+    }, (error) => {
+        console.log(error.text);
+    });
+};
 
 const Contact = () => {
   const [formData, setFormData] = useState({
@@ -28,6 +40,12 @@ const Contact = () => {
     setFormSubmitStatus('Form submitted successfully!');
   };
 
+  const Submit = () => {
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("phone").value = "";
+    document.getElementById("txta").value = "";
+  }
   return (
     <section className={styles.contact} id="contact">
       <h1 className='heading'>contact us</h1>
@@ -36,13 +54,14 @@ const Contact = () => {
         <div className={styles.image}>
           <img src={contactImg} alt />
         </div>
-        <form onSubmit={handleSubmit}>
+        <form id='form_id' onSubmit={sendEmail}>
           <h3>send us a message</h3>
           <input
             type="text"
             placeholder="name"
             className={styles.box}
             name="name"
+            id='name'
             value={formData.name}
             onChange={handleInputChange}
           />
@@ -51,6 +70,7 @@ const Contact = () => {
             placeholder="email"
             className={styles.box}
             name="email"
+            id='email'
             value={formData.email}
             onChange={handleInputChange}
           />
@@ -59,6 +79,7 @@ const Contact = () => {
             placeholder="phone number"
             className={styles.box}
             name="phone"
+            id='phone'
             value={formData.phone}
             onChange={handleInputChange}
           />
@@ -68,10 +89,11 @@ const Contact = () => {
             cols="30"
             rows="10"
             name="message"
+            id='txta'
             value={formData.message}
             onChange={handleInputChange}
           ></textarea>
-          <button type="submit" className='btn'>
+          <button type="submit" className='btn' onClick={Submit}>
             <span className='text1'>send message</span>
             <span className='text2' aria-hidden="true">
               send message
